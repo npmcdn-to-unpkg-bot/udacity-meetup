3647404aa9afc00f452239a99d4045aa984f3cf6
Adrian Carriger <mail@adrianandnicole.com>
2016-06-21 09:23:13 -0700

    Add app component prefix "as"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  selector: 'app-root',
+  selector: 'as-app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
