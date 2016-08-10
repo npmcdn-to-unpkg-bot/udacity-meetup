e212c94f195833aa16f54b0540f2548140017614
Adrian Carriger <mail@adrianandnicole.com>
2016-08-10 11:47:12 -0700

    Fix: birthday format, search bar fixed positioning -Search bar wasn't using a dynamic minScroll point to set `position: fixed`

--- src/app/front/+home/home.component.ts
+++ src/app/front/+home/home.component.ts
@@ -4,7 +4,6 @@ import { AutofocusDirective } from '../../shared/directives/autofocus.directive'
 import { ApiService } from '../../shared/services/api.service';
 import { GlobalEventsService } from '../../shared/services/global-events.service';
 import { PaginatePipe, IPaginationInstance, PaginationService } from 'ng2-pagination';
-
 declare let Vimeo: any;
 
 @Component({
@@ -15,56 +14,36 @@ declare let Vimeo: any;
   directives: [EventsComponent, AutofocusDirective],
   providers: [PaginationService]
 })
-export class HomeComponent implements OnInit, AfterViewChecked, OnDestroy  {
+export class HomeComponent implements OnInit, AfterViewChecked, OnDestroy {
   @ViewChild('videoContainer') videoContainer;
-  
-  public video: any;
-  public searchFixed: boolean = false;
+  @ViewChild('searchContainer') searchContainer;
+  public video:any;
+  public searchFixed:boolean = false;
   public events: Array<Object>;
-  public vcHeight: number;
-  public lastManuelFocus: number;
-  public loaded: boolean = false;
-  
-  private intervalReference: number;
-  private yPos: number = 0;
-  private minScroll: number = 999999;
-  private scrollUpdateNeeded: boolean = false;
+  public vcHeight:number;
+  public lastManuelFocus:number;
+  public loaded:boolean = false;
+  private intervalReference:number;
+  private yPos:number = 0;
+  private minScroll:number = 999999;
+  private scrollUpdateNeeded:boolean = false;
   
   constructor(
     public globalEventsService: GlobalEventsService,
     private apiService: ApiService) {}
-
-  private getDimensions(): void {
-    let winHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    // This is how far down the search bar is before any scrolling
-    let vcTop = this.videoContainer.nativeElement.offsetTop;
-    // search bar height is 22px
-    this.minScroll = 260 + vcTop + 22 - (0.017 * winHeight);
-    this.vcHeight = this.videoContainer.nativeElement.offsetHeight;
-    this.updateFixed();
-  }
-
-  private updateFixed(): void {
-    if (this.yPos >= this.minScroll) {
-      this.searchFixed = true;
-    } else {
-      this.searchFixed = false;
-    }
-  }
   
-  public onInput(): void {
+  public onInput():void {
+    // Moves scroll position on next digest cycle
     this.scrollUpdateNeeded = true;
   }
 
-  public manuelFocus(): void {
+  public manuelFocus():void {
     let currentDate = new Date;
     this.lastManuelFocus = currentDate.getTime();
   }
 
-  ngOnInit() {
-    this.apiService.checkEmail('meow@dog.com').subscribe( data => {
-      
-    });
+  public ngOnInit():void {
+    // Background Video
     let iframe = document.getElementById('hero-video');
     this.video = new Vimeo.Player(iframe);
     this.video.on('loaded', data => {
@@ -72,11 +51,11 @@ export class HomeComponent implements OnInit, AfterViewChecked, OnDestroy  {
           this.loaded = true;
         }, 2500);
     });
-    
+    // Restart video periodically
     this.intervalReference = setInterval( () => {
       this.video.setCurrentTime(231);
     }, 15000);
-
+    // Set listeners related to fixing the search bar
     this.getDimensions();
     this.globalEventsService.elementsCollection['resize'].emitter$.subscribe(data => {
       this.getDimensions();
@@ -84,25 +63,64 @@ export class HomeComponent implements OnInit, AfterViewChecked, OnDestroy  {
     this.globalEventsService.elementsCollection['scroll'].emitter$.subscribe( () => {
       this.yPos = document.body.scrollTop;
       this.updateFixed();
-    });
-    
+    }); 
   }
 
-  ngAfterViewChecked() {
-    // http://stackoverflow.com/a/35493028/5357459
+  public ngAfterViewChecked():void {
+    this.updateScrollPosition();
+  }
+
+  public ngOnDestroy():void {
+    clearInterval(this.intervalReference);
+  }
+
+  /**
+   * Get dimensions related to fixing the search bar
+   */
+  private getDimensions():void {
+    // Window height
+    let winHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    // Pixels below the top of the page (Desktop: 60px)
+    let vcTop = this.videoContainer.nativeElement.offsetTop;
+    // Pixels below `videoContainer` (Desktop: 260px)
+    let searchContainerTop = this.searchContainer.nativeElement.offsetTop;
+    // Minimum scroll distance before switching to a fixed search bar
+    this.minScroll = searchContainerTop + vcTop + 22 - (0.017 * winHeight);
+    this.vcHeight = this.videoContainer.nativeElement.offsetHeight;
+    this.updateFixed();
+  }
+
+  /**
+   * Checks if the search bar should be fixed,
+   * then changes a variable that changes the 
+   * class via ngClass in the Html 
+   */
+  private updateFixed():void {
+    if (this.yPos >= this.minScroll) {
+      this.searchFixed = true;
+    } else {
+      this.searchFixed = false;
+    }
+  }
+
+  /**
+   * Move scroll position on input (workaround)
+   * 
+   * If the search bar recieves input (via `onInput()`),
+   * then this function is continually called
+   * (on each digest cycle via `ngAfterViewChecked()`)
+   * to update the document's scroll position 
+   * until the position actually changes.
+   * http://stackoverflow.com/a/35493028/5357459
+   */
+  private updateScrollPosition():void {
     if (this.scrollUpdateNeeded) {
       if (document.body.scrollTop === this.vcHeight) {
         this.scrollUpdateNeeded = false;
       } else {
         document.body.scrollTop = this.vcHeight;
       }
-    }    
+    }
   }
-
-  ngOnDestroy() {
-    clearInterval(this.intervalReference);
-  }
-
-  
 
 }
--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -22,20 +22,8 @@ export class ApiService {
     this.preMedia$ = new BehaviorSubject(this.media);
   }
 
-  checkEmail(email) {
-    let api_key = 'pubkey-b7fb0c2576ed98d64fafc660d9ad42e2';
-    let url = 'https://api.mailgun.net/v2/address/validate?api_key=' + api_key;
-    let searchParams = this.searchParamsService.transform( {address: email} );
-    return this.http.get(url, {
-        search: searchParams
-    })
-    .map( (responseData) => {
-      return responseData.json();
-    });
-  }
-
-  observe(paramsObj, apiLocation) {
-    let url = 'https://www.eventbriteapi.com/v3/' + apiLocation + '/?token=S6S7G427VEDSLNEQRE6B';
+  public observe(paramsObj, apiLocation) {
+    let url:string = 'https://www.eventbriteapi.com/v3/' + apiLocation + '/?token=S6S7G427VEDSLNEQRE6B';
     let searchParams = this.searchParamsService.transform(paramsObj);
     return this.http.get(url, {
         search: searchParams
@@ -45,7 +33,7 @@ export class ApiService {
     });
   }
 
-  getCordinates(address) {
+  public getCordinates(address) {
     let url = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBE1Bb86PEGx-11LahjWCZS2cFOWMpNseI';
     let searchParams = this.searchParamsService.transform({address:address});
     return this.http.get(url, {
