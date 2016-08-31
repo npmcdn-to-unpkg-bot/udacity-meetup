ad2fdbbddeb97f744e786283f2bd53ae13d0ad00
Adrian Carriger <mail@adrianandnicole.com>
2016-08-31 10:35:57 -0700

    chore: upgrade angular-cli to webpack.8

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,3 +1,5 @@
+import { browser, element, by } from 'protractor/globals';
+
 export class UdacityMeetupPage {
   navigateTo() {
     return browser.get('/');
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import "./polyfills.ts";
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+
 
 import { APP_ROUTER_PROVIDERS } from './app/app.routes';
 import { disableDeprecatedForms, provideForms } from '@angular/forms';
